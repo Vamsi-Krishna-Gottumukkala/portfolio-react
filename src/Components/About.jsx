8821b7c14da867c87fa21584eaf869d3a5bc1ae6
Vamsi-Krishna-Gottumukkala <gvamsikrishna.0420@gmail.com>
2024-05-31 21:11:53 +0530

    added about page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import './about.css'
 
 function About() {
   return (
-    <div>
-      <h1>aboout</h1>
+    <div className='amain'>
+      <div className="aleft">
+        <img src="photo_me.png" alt="my_photo" width="360px" className='my_photo'/>
+      </div>
+      <div className="aright">
+        <div className="abtme">About <span>Me</span></div>
+        <div className="mtr">
+            <p className='about'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Necessitatibus, accusantium placeat. Consectetur labore repudiandae 
+                dicta id in provident non magnam rerum cum consequuntur perspiciatis, explicabo, 
+                voluptatum harum aspernatur! Cupiditate, doloremque.
+            </p>
+            <div className="resume">
+                <a href="Resume.pdf" target='_blank'>Download Resume</a>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
