d2db347d2c4059d60122ceceda02b20f2fc22d84
Vamsi-Krishna-Gottumukkala <gvamsikrishna.0420@gmail.com>
2024-05-31 19:04:46 +0530

    added homepage

---
+++ src/Components/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+  return (
+    <div className='hmain'>
+        <div className="hleft">
+            <div className="hello">Hello, it's Me</div>
+            <div className="name">Vamsi Krishna</div>
+            <div className="intro">I am a <span>Frontend Developer</span></div>
+        </div>
+        <div className="hright">
+            <div className="mg">
+                <img src="stock.jpg" alt="photo-jpg" width="620px" className='ig'/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
