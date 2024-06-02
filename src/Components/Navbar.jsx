20caa210eb15d4f2679bb37d4ac3e21c0ffa1b5c
Vamsi-Krishna-Gottumukkala <gvamsikrishna.0420@gmail.com>
2024-06-02 19:43:41 +0530

    highlights the displayed tab in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,15 +3,26 @@ import './navbar.css'
 import { Link } from 'react-scroll'
 
 function Navbar() {
+
   return (
       <aside>
-        <img src="VK-logo.png" alt="vk logo" height="82px"/>
+        <img src="VK-logo.png" alt="vk-logo" height="82px"/>
         <div className="links">
-          <Link className="al" to="home" spy={true} smooth={true} offset={0} duration={500}>home</Link>
-          <Link className="al" to="about" spy={true} smooth={true} offset={0} duration={500}>about</Link>
-          <Link className="al" to="skills" spy={true} smooth={true} offset={0} duration={500}>skills</Link>
-          <Link className="al" to="home" spy={true} smooth={true} offset={0} duration={500}>work</Link>
-          <Link className="al" to="home" spy={true} smooth={true} offset={0} duration={500}>contact</Link>
+          <div className="sections">
+            <Link to="home" spy={true} smooth={true} offset={0} duration={500}>home</Link>
+          </div>
+          <div className="sections">
+            <Link to="about" spy={true} smooth={true} offset={0} duration={500} >about</Link>
+          </div>
+          <div className="sections">
+            <Link to="skills" spy={true} smooth={true} offset={0} duration={500} >skills</Link>
+          </div>
+          <div className="sections">
+            <Link to="/" spy={true} smooth={true} offset={0} duration={500} >work</Link>
+          </div>
+          <div className="sections">
+            <Link to="/" spy={true} smooth={true} offset={0} duration={500} >contact</Link>
+          </div>
         </div>
       </aside>
   )
