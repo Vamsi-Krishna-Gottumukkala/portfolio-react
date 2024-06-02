287614fcec26b9e587d1f05732ee3e0c4ee17987
Vamsi-Krishna-Gottumukkala <gvamsikrishna.0420@gmail.com>
2024-06-02 20:12:16 +0530

    added motion text

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import './home.css'
+import Typed from 'typed.js';
 
 function Home() {
+
+  const multitext = React.useRef(null);
+
+  React.useEffect(() => {
+    const typed = new Typed(multitext.current, {
+      strings: ['Fullstack Devloper', 'Ui/Ux Designer', 'Gamer'],
+      typeSpeed: 100,
+      backSpeed: 80,
+      backDelay: 1500,
+      loop : true,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+
   return (
     <div className='hmain' id='home'>
         <div className="hleft">
             <div className="hello">Hello, it's Me</div>
             <div className="name">Vamsi Krishna</div>
-            <div className="intro">I am a <span>Frontend Developer</span></div>
+            <div className="intro">I am a <span ref={multitext}>Frontend Developer</span></div>
         </div>
         <div className="hright">
             <div className="mg">
